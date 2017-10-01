43930f2300099b233205e1075a3b24f8f3c08c0b
Jakob Buis <jakob@jakobbuis.nl>
2017-10-01 12:57:58 +0200

    use bulma (and SCSS)

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,9 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+@import 'node_modules/bulma/bulma.sass';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
