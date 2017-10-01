92075656dc9606f930a61d27c9720377083eeddd
Jakob Buis <jakob@jakobbuis.nl>
2017-10-01 13:45:53 +0200

    fleshed out basic, non-operable navigation

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
   <div id="app">
-    <menu></menu>
+    <Navigation></Navigation>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Navigation from './components/Navigation';
 
 export default {
     name: 'app',
+    components: { Navigation },
 };
 </script>
 
 <style lang="scss">
 @import 'node_modules/bulma/bulma.sass';
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,45 @@
+<template>
+    <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <h1>Bolknoms</h1>
+
+            <div class="navbar-burger">
+
+            </div>
+        </div>
+
+        <div class="navbar-menu">
+            <div class="navbar-start">
+                <a href="#" class="navbar-item">Aanmelden</a>
+                <a href="#" class="navbar-item">Statistieken</a>
+                <div href="#" class="navbar-item has-dropdown is-hoverable">
+                    <a href="#" class="navbar-link">Administratie</a>
+                    <div class="navbar-dropdown">
+                        <a href="#" class="navbar-item">Gebruikers</a>
+                        <a href="#" class="navbar-item">Maaltijden</a>
+                    </div>
+                </div>
+            </div>
+            <div class="navbar-end">
+                <a href="#" class="navbar-item">User photo</a>
+                <a href="#" class="navbar-item" title="Uitloggen">
+                    <i class="fa fa-power-off" aria-hidden="true"></i>
+                </a>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style scoped>
+h1 {
+    display: inline;
+    font-weight: bold;
+    font-size: x-large;
+    padding: 0.25em 1em 0 1em;
+}
+</style>
