d47260aabad1171bcfbb175e8613666eb32adf6f
Jakob Buis <jakob@jakobbuis.nl>
2017-10-14 20:10:15 +0200

    use larger pictures for future usage

--- src/services/User.js
+++ src/services/User.js
@@ -2,6 +2,6 @@ export default {
 
     picture() {
         const oauth = window.store.state.oauth;
-        return `https://people.debolk.nl/persons/${oauth.user.username}/photo/128/128?access_token=${oauth.token.access_token}`;
+        return `https://people.debolk.nl/persons/${oauth.user.username}/photo/256/256?access_token=${oauth.token.access_token}`;
     },
 };
